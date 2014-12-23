45643a4726cac79a390911923ef787b650a16c8c
MFQ <softmfq@gmail.com>
2014-12-23 15:43:48 +0500

    AppView is added

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -1,14 +1,14 @@
-define(function(require, export, module){
+define(function(require, exports, module){
 	var Views = require('famous/core/View');
 	var Surface = require('famous/core/Surface');
 	var Transform = require('famous/core/Transform');
 	var StateModifier = require('famous/modifiers/StateModifier');
 
 	function AppView () {
-		this.apply(this, arguments)
+ 		Views.apply(this, arguments);
 	}
 
-	AppView.prototype = Object.create(View.prototype);
+	AppView.prototype = Object.create(Views.prototype);
 	AppView.prototype.constructor = AppView;
 
 	AppView.DEFAULT_OPTIONS = {};
